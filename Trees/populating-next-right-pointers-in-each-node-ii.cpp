05bf42b535dcbec9883219bf59c64004beba6aaf
SparshJain2000 <jainsparsh0801@gmail.com>
2020-07-20 09:47:00 +0530

    Trees: poluting next right pointer i & ii

---
+++ Trees/populating-next-right-pointers-in-each-node-ii.cpp
@@ -0,0 +1,60 @@
+//Problem Statement
+/*
+Given a binary tree
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+
+Initially, all next pointers are set to NULL.
+*/
+#include <bits/stdc++.h>
+// Definition for a Node.
+class Node
+{
+public:
+    int val;
+    Node *left;
+    Node *right;
+    Node *next;
+
+    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
+
+    Node(int _val, Node *_left, Node *_right, Node *_next)
+        : val(_val), left(_left), right(_right), next(_next) {}
+};
+class Solution
+{
+public:
+    Node *connect(Node *root)
+    {
+        if (root == NULL)
+            return NULL;
+        queue<Node *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            queue<Node *> q1;
+            while (!q.empty())
+            {
+                Node *x = q.front();
+                q.pop();
+                if (q.empty())
+                    x->next = NULL;
+                else
+                    x->next = q.front();
+                if (x->left)
+                    q1.push(x->left);
+                if (x->right)
+                    q1.push(x->right);
+            }
+            q = q1;
+        }
+        return root;
+    }
+};
