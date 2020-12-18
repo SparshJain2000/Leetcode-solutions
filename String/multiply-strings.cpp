21b96240290375264a9f1358e0b6747e16ce868e
SparshJain2000 <jainsparsh0801@gmail.com>
2020-12-19 00:41:26 +0530

    String : multiply string

--- String/multiply-strings.cpp
+++ String/multiply-strings.cpp
@@ -3,6 +3,7 @@
 Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
 */
 #include <bits/stdc++.h>
+using namespace std;
 class Solution
 {
 private:
