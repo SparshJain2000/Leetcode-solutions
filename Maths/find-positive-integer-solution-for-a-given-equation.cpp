484d128aed77faf07c6da24d274a898f8e2d25a4
SparshJain2000 <jainsparsh0801@gmail.com>
2021-02-08 00:59:28 +0530

    Maths : pos solution of equation

---
+++ Maths/find-positive-integer-solution-for-a-given-equation.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+// This is the custom function interface.
+// You should not implement it, or speculate about its implementation
+class CustomFunction {
+   public:
+    // Returns f(x, y) for any given positive integers x and y.
+    // Note that f(x, y) is increasing with respect to both x and y.
+    // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+    int f(int x, int y);
+};
+class Solution {
+   public:
+    vector<vector<int>> findSolution(CustomFunction& customfunction, int z) {
+        vector<vector<int>> ans;
+        int y = 1000;
+        for (int x = 1; x < 1001; ++x) {
+            while (y > 1 && customfunction.f(x, y) > z) y--;
+            if (customfunction.f(x, y) == z) ans.push_back(vector<int>({x, y}));
+        }
+        return ans;
+    }
+};
