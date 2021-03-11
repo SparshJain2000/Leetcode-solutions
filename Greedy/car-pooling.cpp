44e2fa261075b3bf191e3468192882c8634aa869
SparshJain2000 <jainsparsh0801@gmail.com>
2021-03-12 01:06:53 +0530

    Greedy : car pooling

--- Greedy/car-pooling.cpp
+++ Greedy/car-pooling.cpp
@@ -8,35 +8,27 @@ Given a list of trips, trip[i] = [num_passengers, start_location, end_location] 
 Return true if and only if it is possible to pick up and drop off all passengers for all the given trips. 
 */
 #include <bits/stdc++.h>
-class Solution
-{
-public:
-    bool carPooling(vector<vector<int>> &trips, int capacity)
-    {
+using namespace std;
+class Solution {
+   public:
+    bool carPooling(vector<vector<int>> &trips, int capacity) {
         map<int, vector<pair<int, int>>> M;
 
-        for (auto i : trips)
-        {
+        for (auto i : trips) {
             M[i[1]].push_back({0, i[0]});
             M[i[2]].push_back({1, i[0]});
         }
 
         int current_capacity = 0;
-        for (auto it = M.begin(); it != M.end(); it++)
-        {
-            for (auto it2 : it->second)
-            {
-                if (it2.first == 0)
-                {
+        for (auto it = M.begin(); it != M.end(); it++) {
+            for (auto it2 : it->second) {
+                if (it2.first == 0) {
                     current_capacity += it2.second;
-                }
-                else
-                {
+                } else {
                     current_capacity -= it2.second;
                 }
             }
-            if (current_capacity > capacity)
-            {
+            if (current_capacity > capacity) {
                 return false;
             }
         }
