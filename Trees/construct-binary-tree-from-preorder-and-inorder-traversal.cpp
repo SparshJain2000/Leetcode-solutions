2d80b3a096eba3f6fef2bbb50c3cf2106b213156
SparshJain2000 <jainsparsh0801@gmail.com>
2021-05-07 00:35:40 +0530

    TREES : construct tree from pre and inorder

--- Trees/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ Trees/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -3,28 +3,26 @@
 
 #include <bits/stdc++.h>
 //Definition for a binary tree node.
-struct TreeNode
-{
+using namespace std;
+struct TreeNode {
     int val;
     TreeNode *left;
     TreeNode *right;
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
-class Solution
-{
-public:
-    TreeNode *buildTree(vector<int> preorder, vector<int> inorder)
-    {
+class Solution {
+   public:
+    TreeNode *buildTree(vector<int> preorder, vector<int> inorder) {
         if (!preorder.size() || !inorder.size())
             return NULL;
-
-        TreeNode *root = new TreeNode(preorder.at(0));
-        int i = find(inorder.begin(), inorder.end(), root->val) - inorder.begin();
+        TreeNode *root = new TreeNode(preorder[0]);
+        int i = 0;
+        for (i = 0; i < inorder.size(); i++)
+            if (inorder[i] == root->val) break;
         vector<int> left(inorder.begin(), inorder.begin() + i);
         vector<int> right(inorder.begin() + i + 1, inorder.end());
 
         root->left = buildTree(vector<int>(preorder.begin() + 1, preorder.begin() + left.size() + 1), left);
-
         root->right = buildTree(vector<int>(preorder.begin() + left.size() + 1, preorder.end()), right);
         return root;
     }
