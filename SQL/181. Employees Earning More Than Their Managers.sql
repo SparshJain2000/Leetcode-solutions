ca7df46a4fabfae0bf3e77155dd225cef7a29095
SparshJain2000 <jainsparsh0801@gmail.com>
2021-07-10 00:01:43 +0530

    SQL : salary more than manager (add method)

--- SQL/181. Employees Earning More Than Their Managers.sql
+++ SQL/181. Employees Earning More Than Their Managers.sql
@@ -1,23 +1,56 @@
-The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
-
-+----+-------+--------+-----------+
-| Id | Name  | Salary | ManagerId |
-+----+-------+--------+-----------+
-| 1  | Joe   | 70000  | 3         |
-| 2  | Henry | 80000  | 4         |
-| 3  | Sam   | 60000  | NULL      |
-| 4  | Max   | 90000  | NULL      |
-+----+-------+--------+-----------+
-Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
-
-+----------+
-| Employee |
-+----------+
-| Joe      |
-+----------+
-select name as 'Employee' from employee e1
-where salary>(select salary from employee e2 where e2.id=e1.managerid);
+/*
+ The Employee table holds all employees including their managers. Every employee has an Id, and there is also a column for the manager Id.
+ 
+ +----+-------+--------+-----------+
+ | Id | Name  | Salary | ManagerId |
+ +----+-------+--------+-----------+
+ | 1  | Joe   | 70000  | 3         |
+ | 2  | Henry | 80000  | 4         |
+ | 3  | Sam   | 60000  | NULL      |
+ | 4  | Max   | 90000  | NULL      |
+ +----+-------+--------+-----------+
+ Given the Employee table, write a SQL query that finds out employees who earn more than their managers. For the above table, Joe is the only employee who earns more than his manager.
+ 
+ +----------+
+ | Employee |
+ +----------+
+ | Joe      |
+ +----------+
+ */
+SELECT
+    NAME AS 'Employee'
+FROM
+    employee e1
+WHERE
+    salary >(
+        SELECT
+            salary
+        FROM
+            employee e2
+        WHERE
+            e2.id = e1.managerid
+    );
 --------------------------------------------------------------------------
-select e1.name as 'Employee' from 
-employee e1 join employee e2
-where e1.managerid=e2.id and e1.salary>e2.salary;
+SELECT
+    e1.name AS 'Employee'
+FROM
+    employee e1
+    JOIN employee e2
+WHERE
+    e1.managerid = e2.id
+    AND e1.salary > e2.salary;
+--------------------------------------------------------------------------
+SELECT
+    NAME AS "Employee"
+FROM
+    Employee e
+WHERE
+    EXISTS (
+        SELECT
+            salary
+        FROM
+            Employee
+        WHERE
+            Id = e.ManagerId
+            AND salary < e.salary
+    );
