e51769246272fea27d59624a09cbce07c8eceedc
SparshJain2000 <jainsparsh0801@gmail.com>
2021-08-18 00:48:23 +0530

    MATHS : convert to base 2

---
+++ Maths/convert-to-base-2.cpp
@@ -0,0 +1,21 @@
+/*
+Problem Statement
+
+Given a number N, return a string consisting of "0"s and "1"s that represents its value in base -2 (negative two).
+The returned string must have no leading zeroes, unless the string is "0".
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+   public:
+    string baseNeg2(int N) {
+        string ans = "";
+        while (N) {
+            ans += to_string(N & 1);
+            N = -(N >> 1);
+        }
+        reverse(ans.begin(), ans.end());
+        return ans == "" ? "0" : ans;
+    }
+};
